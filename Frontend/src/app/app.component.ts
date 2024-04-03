5dea82c50a097bc1a1cf0154558bbac6eb20026d
kalelkar-noman <kalelkarnoman014@gmail.com>
2024-04-03 22:40:05 +0530

    routes added in frontend

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { RegistrationComponent } from './Pages/registration/registration.component';
 import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegistrationComponent, NgIf],
+  imports: [RouterOutlet,RouterLink, RegistrationComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
